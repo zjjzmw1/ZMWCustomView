271b7c07b5d2608fcd6593f1fd1553687a4c1ec2
zhangmingwei <zjjzmw1@163.com>
2016-07-11 16:48:38 +0800

    tableView 的优化，添加了各种常用的方法

--- ZMWCustomView/HomeViewController.m
+++ ZMWCustomView/HomeViewController.m
@@ -58,16 +58,28 @@
 
 /// 初始化表格
 -(void)initTableView {
-    self.tableView = [[UITableView alloc]initWithFrame:self.view.frame style:UITableViewStylePlain];
+    // UITableViewStyleGrouped 的话，表格上下都有一个满格的横线。UITableViewStylePlain 没有。
+    self.tableView = [[UITableView alloc]initWithFrame:self.view.frame style:UITableViewStyleGrouped];
+    /// 但是设置成 UITableViewStyleGrouped 后，表格上面有留白，可以通过 设置 heightForHeaderInSection 高度来 去掉留白.
+    
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     [self.view addSubview:self.tableView];
-    self.tableView.separatorInset = UIEdgeInsetsMake(0, 20, 0, 0);//线的位置
+    // 中间线的颜色、样式
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    self.tableView.separatorColor = [UIColor lightGrayColor];
+    // 中间线的位置
+    self.tableView.separatorInset = UIEdgeInsetsMake(0, 20, 0, 0);
+    // 内容少于一屏的时候，去掉多余的中间的线。
     UIView *footerV = [[UIView alloc]initWithFrame:CGRectZero];
     [self.tableView setTableFooterView:footerV];
 }
 
 #pragma mark - Table view data source
+/// 可以去掉 表格设置 UITableViewStyleGrouped 后 默认的 上面的留白
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 0.0001;
+}
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
