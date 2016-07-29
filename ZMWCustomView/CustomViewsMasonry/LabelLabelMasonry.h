3fa58938e459e53adb3ad41a670676426a130ec9
zhangmingwei <zjjzmw1@163.com>
2016-07-29 16:41:10 +0800

    添加masonry库，和自定义masonryView的文件夹

---
+++ ZMWCustomView/CustomViewsMasonry/LabelLabelMasonry.h
@@ -0,0 +1,13 @@
+//
+//  LabelLabelMasonry.h
+//  ZMWCustomView
+//
+//  Created by speedx on 16/7/29.
+//  Copyright © 2016年 shandandan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LabelLabelMasonry : UIView
+
+@end
---
+++ ZMWCustomView/CustomViewsMasonry/LabelLabelMasonry.m
@@ -0,0 +1,21 @@
+//
+//  LabelLabelMasonry.m
+//  ZMWCustomView
+//
+//  Created by speedx on 16/7/29.
+//  Copyright © 2016年 shandandan. All rights reserved.
+//
+
+#import "LabelLabelMasonry.h"
+
+@implementation LabelLabelMasonry
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
